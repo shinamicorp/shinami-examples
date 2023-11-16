5df549a0334a31d723a547412501d6376cac5063
Aaron Tietz <aarontietz@Aarons-Laptop.local>
2023-11-15 16:52:53 -0800

    update Gas Station sample code

--- gas-station/typescript/src/sponsored_examples.ts
+++ gas-station/typescript/src/sponsored_examples.ts
@@ -1,163 +1,158 @@
-import { SuiClient } from "@mysten/sui.js/client";
 import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
 import { TransactionBlock } from "@mysten/sui.js/transactions";
-import { rpcClient } from "typed-rpc";
+import { GasStationClient, createSuiClient, buildGaslessTransactionBytes } from "@shinami/clients";
 
-// The initiator of the transaction
-const SENDER_ADDRESS = "<SUI_ADDRESS>";
-const SENDER_PRIVATE_KEY = "<PRIVATE_KEY>";
-const SENDER_RECOVERY_PHRASE = "<RECOVERY_PHRASE>";
+// 1. Copy your testnet Gas Station and Node Service key values
+const GAS_AND_NODE_TESTNET_ACCESS_KEY = "{{gasAndNodeServiceTestnetAccessKey}}";
 
-// gas budget, in MIST
-const GAS_BUDGET = 5000000;
+// 2. Set up your Gas Station and Node Service clients
+const nodeClient = createSuiClient(GAS_AND_NODE_TESTNET_ACCESS_KEY);
+const gasStationClient = new GasStationClient(GAS_AND_NODE_TESTNET_ACCESS_KEY);
 
-// Gas Station endpoint:
-const SPONSOR_RPC_URL = "https://api.shinami.com/gas/v1/<GAS_ACCESS_KEY>";
+// 3. Generate a new KeyPair to represent the sender
+let keyPair = new Ed25519Keypair();
+const SENDER_ADDRESS = keyPair.toSuiAddress();
 
-// Sui Node endpoint:
-const suiClient = new SuiClient({
-  // Sui Node endpoint:
-  url: "https://api.shinami.com/node/v1/<NODE_ACCESS_KEY>",
+// 4. Generate the TransactionKind for sponsorship as a Base64 encoded string
+let gaslessPayloadBase64 = await buildGaslessTransactionBytes({
+  sui: nodeClient,
+  build: async (txb) => {
+    txb.moveCall({
+      target: "0xfa0e78030bd16672174c2d6cc4cd5d1d1423d03c28a74909b2a148eda8bcca16::clock::access",
+      arguments: [txb.object('0x6')]
+    });
+  }
 });
 
-// Create the sender's address key pair from the recovery phrase
-// const keyPair = Ed25519Keypair.deriveKeypair(SENDER_RECOVERY_PHRASE);
+// 5. Set your gas budget, in MIST
+const GAS_BUDGET = 5_000_000;
 
-// Create the sender's address key pair from the sender's private key
-const buf = Buffer.from(SENDER_PRIVATE_KEY, "base64");
-const keyPair = Ed25519Keypair.fromSecretKey(buf.slice(1));
+// 6. Send the TransactionKind to Shinami Gas Station for sponsorship
+let sponsoredResponse = await gasStationClient.sponsorTransactionBlock(
+  gaslessPayloadBase64,
+  SENDER_ADDRESS,
+  GAS_BUDGET
+);
 
-// Setup for issuing json rpc calls to the gas station for sponsorship. We use typed-rpc typescript lib here.
-interface SponsoredTransaction {
-  txBytes: string;
-  txDigest: string;
-  signature: string;
-  expireAtTime: number;
-  expireAfterEpoch: number;
-}
-type SponsoredTransactionStatus = "IN_FLIGHT" | "COMPLETE" | "INVALID";
+// 7. The transaction should be sponsored now, so its status will be "IN_FLIGHT"
+let sponsoredStatus = await gasStationClient.getSponsoredTransactionBlockStatus(
+  sponsoredResponse.txDigest
+);
+console.log("Transaction Digest:", sponsoredResponse.txDigest);
+console.log("Sponsorship Status:", sponsoredStatus);
 
-interface SponsorRpc {
-  gas_sponsorTransactionBlock(
-    txBytes: string,
-    sender: string,
-    gasBudget: number
-  ): SponsoredTransaction;
-  gas_getSponsoredTransactionBlockStatus(
-    txDigest: string
-  ): SponsoredTransactionStatus;
-}
-const sponsor = rpcClient<SponsorRpc>(SPONSOR_RPC_URL);
+// 8. Sign the full transaction payload with the sender's key.
+let senderSig = await TransactionBlock.from(sponsoredResponse.txBytes).sign(
+  {
+    signer: keyPair,
+  }
+);
 
-// Different examples of programmable transaction blocks that can be crafted.
-const progTxnTransfer = () => {
-  // The receiver of a transaction
-  const RECIPIENT_ADDRESS = "<SUI_ADDRESS>";
+// 9. Send the full transaction payload, along with the gas owner 
+//     and sender's signatures for execution on the Sui network
+let executeResponse = await nodeClient.executeTransactionBlock({
+  transactionBlock: sponsoredResponse.txBytes,
+  signature: [senderSig.signature, sponsoredResponse.signature],
+  options: { showEffects: true },
+  requestType: "WaitForLocalExecution",
+});
 
-  // For transferring objects transaction
-  const OBJECT_TO_SEND = "<OBJECT_ID>";
+console.log("Transaction Digest:", executeResponse.digest);
+console.log("Execution Status:", executeResponse.effects?.status.status);
 
-  const txb = new TransactionBlock();
-  // create a programmable transaction to send an object from the sender to the recipient
-  txb.transferObjects(
-    [txb.object(OBJECT_TO_SEND)],
-    txb.pure(RECIPIENT_ADDRESS)
-  );
-  return txb;
-};
 
-const progTxnSplit = () => {
-  const COIN_TO_SPLIT = "<COIN_OBJECT_ID>";
 
-  const txb = new TransactionBlock();
-  const [coin1, coin2] = txb.splitCoins(txb.object(COIN_TO_SPLIT), [
-    txb.pure(10000),
-    txb.pure(20000),
-  ]);
-  // each new object created in a transaction must be sent to an owner
-  txb.transferObjects([coin1, coin2], txb.pure(SENDER_ADDRESS));
-  return txb;
-};
+// -- Other TransactionBlock examples  -- //
 
-const progTxnMerge = () => {
-  const COIN_TARGET = "<COIN_OBJECT_ID>";
-  const COIN_SOURCE1 = "<COIN_OBJECT_ID>";
-  const COIN_SOURCE2 = "<COIN_OBJECT_ID>";
-  const txb = new TransactionBlock();
-  txb.mergeCoins(txb.object(COIN_TARGET), [
-    txb.object(COIN_SOURCE1),
-    txb.object(COIN_SOURCE2),
-  ]);
-  return txb;
-};
+//  Create two new small coins by taking MIST from a larger one.
+//    If you ask for testnet Sui for an account you have the 
+//    private key or passphrase to, you can split two coins from that.
+//
+//
+// const COIN_TO_SPLIT = "{{coinObjectId}}";
+// let gaslessPayloadBase64 = await buildGaslessTransactionBytes({
+//   sui: nodeClient,
+//   build: async (txb) => {
+//     const [coin1, coin2] = txb.splitCoins(txb.object(COIN_TO_SPLIT), [
+//       txb.pure(10000),
+//       txb.pure(20000),
+//     ]);
+//     // each new object created in a transaction must be sent to an owner
+//     txb.transferObjects([coin1, coin2], txb.pure(SENDER_ADDRESS));
+//   }
+// });
 
-const progTxnMoveCall = () => {
-  const txb = new TransactionBlock();
-  txb.moveCall({
-    target: "<PACKAGE_ADDRESS>::<MODULE_NAME>::<METHOD_NAME>",
-    arguments: [
-      // the custom args to this move call
-      //txb.pure(<PURE_VALUE>),
-      //txb.pure(<PURE_VALUE>),
-      //txb.object("<OBJECT_ID>")
-      //...
-    ],
-  });
-  return txb;
-};
 
-const sponsorTransactionE2E = async () => {
-  // get the gasless TransactionBlock for the desired programmable transaction
-  const gaslessTxb = progTxnTransfer();
-  //const gaslessTxb = progTxnSplit();
-  //const gaslessTxb = progTxnMerge();
-  //const gaslessTxb = progTxnMoveCall();
 
-  // generate the bcs serialized transaction data without any gas object data
-  const gaslessPayloadBytes = await gaslessTxb.build({
-    client: suiClient,
-    onlyTransactionKind: true,
-  });
+//  Transfer one or more object(s) owned by the sender address to the recipient
+//    An easy example is a small coin you created with the above transaction.
+//
+//
+// const RECIPIENT_ADDRESS = "{{recipientSuiAddress}}";
+// const OBJECT_TO_SEND = "{{objectId}}";
+// let gaslessPayloadBase64 = await buildGaslessTransactionBytes({
+//   sui: nodeClient,
+//   build: async (txb) => {
+//     txb.transferObjects(
+//       [txb.object(OBJECT_TO_SEND)],
+//       txb.pure(RECIPIENT_ADDRESS)
+//     );
+//   }
+// });
 
-  // convert the byte array to a base64 encoded string to return
-  const gaslessPayloadBase64 = btoa(
-    gaslessPayloadBytes.reduce(
-      (data, byte) => data + String.fromCharCode(byte),
-      ""
-    )
-  );
 
-  // Send the gasless programmable payload to Shinami Gas Station for sponsorship
-  const sponsoredResponse = await sponsor.gas_sponsorTransactionBlock(
-    gaslessPayloadBase64,
-    SENDER_ADDRESS,
-    GAS_BUDGET
-  );
 
-  // The transaction should be sponsored now, so its status will be "IN_FLIGHT"
-  const sponsoredStatus = await sponsor.gas_getSponsoredTransactionBlockStatus(
-    sponsoredResponse.txDigest
-  );
-  console.log("Sponsorship Status:", sponsoredStatus);
+//  Merge one or more smaller coins into another, destroying the small coin(s)
+//    and increasing the value of the large one.
+//
+//
+// const TARGET_COIN = "{{targetCoin}}";
+// const COIN_SOURCE1 = "{{coinSource1}}";
+// const COIN_SOURCE2 = "{{coinSource2}}";
+// let gaslessPayloadBase64 = await buildGaslessTransactionBytes({
+//   sui: nodeClient,
+//   build: async (txb) => {
+//     txb.mergeCoins(txb.object(TARGET_COIN), [
+//       txb.object(COIN_SOURCE1),
+//       txb.object(COIN_SOURCE2),
+//     ]);
+//   }
+// });
 
-  // The sponsoredReponse will contain the full transaction payload and the signature of the gas object owner. To send it off for execution,
-  // the full transaction payload still needs to be signed by the sender of the transaction.
 
-  // Sign the full transaction payload with the sender's key.
-  const senderSig = await TransactionBlock.from(sponsoredResponse.txBytes).sign(
-    {
-      signer: keyPair,
-    }
-  );
+// -- A different way to generate gasless transaction bytes as a base64 encoded string -- //
+// Replace step 4 above with:
+// const txb = new TransactionBlock();
+// txb.moveCall({
+//   target: "0xfa0e78030bd16672174c2d6cc4cd5d1d1423d03c28a74909b2a148eda8bcca16::clock::access",
+//   arguments: [txb.object('0x6')]
+// });
 
-  // Send the full transaction payload, along with the gas owner and sender's signatures for execution on the sui network
-  const executeResponse = await suiClient.executeTransactionBlock({
-    transactionBlock: sponsoredResponse.txBytes,
-    signature: [senderSig.signature, sponsoredResponse.signature],
-    options: { showEffects: true },
-    requestType: "WaitForLocalExecution",
-  });
-  console.log("Execution Status:", executeResponse.effects?.status.status);
-};
+//  generate the bcs serialized transaction data without any gas object data
+// const gaslessPayloadBytes = await txb.build({ client: nodeClient, onlyTransactionKind: true});
 
-sponsorTransactionE2E();
+//  convert the byte array to a base64 encoded string
+// const gaslessPayloadBase64 = btoa(
+//     gaslessPayloadBytes
+//         .reduce((data, byte) => data + String.fromCharCode(byte), '')
+// );
+
+
+
+// -- Generate a KeyPair from a private key or passphrase -- //
+
+//   Create the sender's address KeyPair from the sender's private key
+//
+//
+// const SENDER_PRIVATE_KEY = "{{privateKey}}";
+// const buf = Buffer.from(SENDER_PRIVATE_KEY, "base64");
+// const keyPairFromSecretKey = Ed25519Keypair.fromSecretKey(buf.slice(1));
+// console.log(keyPairFromSecretKey.toSuiAddress());
+
+
+//   Create the sender's address KeyPair from the sender's recovery phrase
+//
+//
+// const SENDER_PASSPHRASE = "{{passphrase}}";
+// const keyPairFromPassphrase = Ed25519Keypair.deriveKeypair(SENDER_PASSPHRASE);
+// console.log(keyPairFromPassphrase.toSuiAddress());
