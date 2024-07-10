2e52f954486f2502346e6f889c4a004cac92afa1
Aaron Tietz <aarontietz@Aarons-Laptop.attlocal.net>
2024-07-10 09:29:13 -0700

    ensure that wallet is initialized before signing, improve comments

--- aptos/typescript/backend_examples/src/invisible_wallet.ts
+++ aptos/typescript/backend_examples/src/invisible_wallet.ts
@@ -17,7 +17,7 @@ import {
 const ALL_SERVICES_TESTNET_ACCESS_KEY = "{{allServicesTestnetAccessKey}}";
 
 // 3. Set up a walletId and its associated secret. Just for the tutorial. Your
-//    app should figure out the best way to manage its wallet Ids and secrets.
+//    app should figure out the best way to manage its wallet IDs and secrets.
 const WALLET_ID = "{{walletID}}";
 const WALLET_SECRET = "{{walletSecret}}";
 
@@ -34,12 +34,12 @@ const signer = new ShinamiWalletSigner(
     keyClient
 );
 
-// 6. Create the Invisible Wallet. We don't need to initialize it on Testnet 
-//    here because the next action for the wallet is a transaction where it is
-//    the sender and your Gas Station fund is the fee payer. This transaction
-//    will initialize it on Testnet.
+// 6. Create the Invisible Wallet. The call to `executeGaslessTransaction` below
+//    would initailize an un-initialized wallet. However, we are explicitly
+//    initializing it now in case you only run the `signAndVerifyTransaction` 
+//    function, because a wallet must be initialized in order to sign a transaction.
 const CREATE_WALLET_IF_NOT_FOUND = true;
-const INITIALIZE_ON_CHAIN = false; 
+const INITIALIZE_ON_CHAIN = true; 
 const walletAddress = await signer.getAddress(CREATE_WALLET_IF_NOT_FOUND, INITIALIZE_ON_CHAIN);
 
 // 7. Sponsor and execute a transaction for the Invisible Wallet. This requires
@@ -58,6 +58,9 @@ console.log("Transaction status:", executedTransaction.vm_status);
 // await signAndVerifyTransaction(signer, aptosClient);
 
 
+/// -- END OF TUTORIAL STEPS -- ///
+
+
 
 // Build a SimpleTransaction that makes a Move call to a Testnet module.
 async function simpleMoveCallTransaction(sender: AccountAddress, withFeePayer = true): Promise<SimpleTransaction> {
@@ -73,19 +76,17 @@ async function simpleMoveCallTransaction(sender: AccountAddress, withFeePayer = 
 
 
 
-// Sign a non-fee-payer transaction with an Invisible Wallet and verify the signature
-async function signAndVerifyTransaction(signer: ShinamiWalletSigner, aptosClient: Aptos): Promise<void> {
+// Sign a non-fee-payer transaction with an Invisible Wallet and verify the signature.
+//  Note: the Invisible Wallet represented by the signer must be initialized on chain 
+//  in order to sign.
+async function signAndVerifyTransaction(onChainWalletSigner: ShinamiWalletSigner, aptosClient: Aptos): Promise<void> {
     // 1. Generate a transaction where an Invisible Wallet is the sender and fee payer. 
-    //    We ensure that the wallet is initialized on chain, because it must be in 
-    //    order to sign the transaction below.
-    const CREATE_WALLET_IF_NOT_FOUND = true;
-    const INITIALIZE_ON_CHAIN = true;
-    const walletAddress = await signer.getAddress(CREATE_WALLET_IF_NOT_FOUND, INITIALIZE_ON_CHAIN);
-    const WITH_FEE_PAYER = false;
-    const transaction = await simpleMoveCallTransaction(walletAddress, WITH_FEE_PAYER);
+    const walletAddress = await onChainWalletSigner.getAddress();
+    const HAS_FEE_PAYER = false;
+    const transaction = await simpleMoveCallTransaction(walletAddress, HAS_FEE_PAYER);
 
     // 2. Sign the transaction
-    const accountAuthenticator = await signer.signTransaction(transaction);
+    const accountAuthenticator = await onChainWalletSigner.signTransaction(transaction);
 
     // 3. Verify the signature.
     const signingMessage = aptosClient.getSigningMessage({ transaction });
