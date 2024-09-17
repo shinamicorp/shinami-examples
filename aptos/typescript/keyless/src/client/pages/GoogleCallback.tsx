02f950f8ddaf9f978109804448fe20fdc9c44b87
main user <aarontietz@Aarons-Laptop.attlocal.net>
2024-09-17 09:09:39 -0700

    update README and comments

--- aptos/typescript/keyless/src/client/pages/GoogleCallback.tsx
+++ aptos/typescript/keyless/src/client/pages/GoogleCallback.tsx
@@ -7,27 +7,34 @@ import { storeKeylessAccount } from "../keyless";
 
 const GoogleCallbackPage = () => {
 
+    // Create a Keyless account and store it in local browser storage so 
+    //  that when the user returns to the site it can be used again (as long
+    //  as the ephmemeral keypair hasn't expired)
     const aptos = new Aptos(new AptosConfig({ network: Network.TESTNET }));
-
     const createKeylessAccount = async (jwt: string, ephemeralKeyPair: EphemeralKeyPair) => {
         const keylessAccount = await aptos.deriveKeylessAccount({
             jwt,
             ephemeralKeyPair,
         });
         storeKeylessAccount(keylessAccount);
-        window.location.href = "/transaction";
     }
 
-
+    // Upon reaching this page when Google responds,
+    // obtain the JWT from the URL.
     const parseJWTFromURL = (url: string): string | null => {
         const urlObject = new URL(url);
         const fragment = urlObject.hash.substring(1);
         const params = new URLSearchParams(fragment);
         return params.get('id_token');
     };
-
     const jwt = parseJWTFromURL(window.location.href);
 
+    // If we have a JWT and a valid ephemeral keypair - used on the home page ('/') 
+    //  for obtaining the nonce used in the OpenID Connect login flow - then 
+    //  create a KeylessAccount for the user and store it in local browser storage.
+    // Finally, change to the transaction page as the user is now logged in.
+    // If we can't obtain a JWT from the URL, send the user back to the home page
+    //  to attempt to log in again.
     if (jwt) {
         const payload = jwtDecode<{ nonce: string }>(jwt);
         const jwtNonce = payload.nonce;
@@ -39,10 +46,11 @@ const GoogleCallbackPage = () => {
         if (!ekp || ekp.nonce !== jwtNonce || ekp.isExpired()) {
             throw new Error("Ephemeral key pair not found or expired");
         }
-        createKeylessAccount(jwt, ekp)
-
+        createKeylessAccount(jwt, ekp);
+        window.location.href = "/transaction";
     } else {
         console.log("Could not obtain JWT from URL!");
+        window.location.href = "/";
     }
 
     return (
